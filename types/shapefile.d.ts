b395a78b0cc968e269d1972e44245b807d76982a
officialn8 <nathanielwpas@gmail.com>
2026-02-02 10:17:57 -0600

    fix(types): add unzipper type declaration

--- types/shapefile.d.ts
+++ types/shapefile.d.ts
@@ -3,3 +3,8 @@ declare module 'shapefile' {
     read: () => Promise<{ done: boolean; value?: { properties: any; geometry: any } }>;
   }>;
 }
+
+declare module 'unzipper' {
+  export function Parse(): any;
+  export function Extract(options: { path: string }): any;
+}
